8e08967112074e7d47b5435b70323c6193b49f1a
Atmospheric_b-10 <atmobsod@gmail.com>
2018-04-02 02:04:23 +0300

    database moved

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,14 @@
 <?php ob_start();?>
 
 <?php
-$db["db_host"]= "localhost";
-$db["db_user"]= "root";
-$db["db_pass"]= "root";
-$db["db_name"]= "cms";
+//$db["db_host"]= "localhost";
+//$db["db_user"]= "root";
+//$db["db_pass"]= "root";
+//$db["db_name"]= "cms";
+$db["db_host"]= "us-cdbr-iron-east-05.cleardb.net";
+$db["db_user"]= "b2e6ad5fe0fe9d";
+$db["db_pass"]= "e9574ae994aa871";
+$db["db_name"]= "heroku_3f21b90598cb615";
 
 foreach($db as $key => $value ){
     define(strtoupper($key), $value);
